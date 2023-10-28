f6a919ee44bc3c6d304fdf1fc9d3a557ca9d1bac
Norufu <dilellojl@gmail.com>
2023-10-28 23:04:23 +0900

    Analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import SearchLayout from "./Components/SearchLayout/SearchLayout";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="App">
       <h1 className="title">PIU Title Hunter</h1>
       <SearchLayout />
+      <Analytics />
     </div>
   );
 }
