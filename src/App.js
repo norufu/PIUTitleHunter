9d6de6850fb7db277a645871bf23ae6d41a722dc
Norufu <dilellojl@gmail.com>
2023-10-28 22:57:24 +0900

    Title / favicon update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import SearchLayout from "./Components/SearchLayout/SearchLayout";
 
-function App({ params, searchParams }) {
+function App() {
   return (
     <div className="App">
       <h1 className="title">PIU Title Hunter</h1>
